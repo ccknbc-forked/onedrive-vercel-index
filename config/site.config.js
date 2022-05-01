8d30510421153b5c97f5b4519cc66e27fc6c97c2
CC康纳百川 <ccknbc@qq.com>
2022-05-01 10:53:56 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/JOOX', '/其他', '/公视非公开', '/公视 创作 版权相关'],
+  protectedRoutes: ['/JOOX', '/其他', '/公视非公开', '/公视版权相关'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:cc@ccknbc.cc',
